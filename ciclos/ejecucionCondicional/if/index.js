8365e5fd00e3dc84934c8e331788ac734ba77eac
Luis Altuve <laltuve@ext.sebbusiness.com>
2024-12-08 19:37:31 -0500

    deleted olds folders

--- ciclos/ejecucionCondicional/if/index.js
+++
@@ -1,9 +0,0 @@
-let nombre = "Nico";
-
-if (nombre === "Diego") {
-  console.log("Hola Diego");
-} else if (nombre === "Nico") {
-  console.log("Hola Nico");
-} else {
-  console.log("Nombre no encontrado");
-}
--- ciclos/loops/for/index.js
+++
@@ -1,9 +0,0 @@
-// for (variable; condición; incremento) {
-//   código a ejecutar
-// }
-
-let list = ["eat", "sleep", "code", "repeat"];
-
-for (let i = 0; i < list.length; i++) {
-  console.log(list[i]);
-}
